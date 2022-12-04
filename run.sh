f2d5c41e52056ca10eb86a37425dbb272f824c98
Reuben Abraham <reuben.abraham20@gmail.com>
2022-12-04 08:17:08 -0500

    output folder rename and gitignore

--- run.sh
+++ run.sh
@@ -1,19 +1,18 @@
 rm LOG.txt
-rm -rf tests/generatedOutput/
-mkdir tests/generatedOutput/
+rm -rf tests/generated_output_shell_script/
+mkdir tests/generated_output_shell_script/
 for i in {1..32}
 do
   printf "*******************************************************************************************************************\n"
   echo *****INPUT $i*****
   printf"\n\n"
   cat tests/testcases/test_$i.txt
-  python3 main.py tests/testcases/test_$i.txt >> tests/generatedOutput/output_$i.txt
+  python3 main.py tests/testcases/test_$i.txt >> tests/generated_output_shell_script/output_$i.txt
   printf "\n\n"
   echo ****OUTPUT $i****
-  cat tests/generatedOutput/output_$i.txt
+  cat tests/generated_output_shell_script/output_$i.txt
   printf "\n\n"
-  diff tests/generatedOutput/output_$i.txt tests/expected_output/output_test_$i.txt >> LOG.txt
-  # printf"\n\n"
+  diff tests/generated_output_shell_script/output_$i.txt tests/expected_output/output_test_$i.txt >> diff_log.txt
   printf "*******************************************************************************************************************\n"
 done
 
